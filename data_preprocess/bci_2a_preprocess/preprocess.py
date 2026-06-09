6d041bedaede01c1754a0ddc60c9ac422a6b76da
fanqi <1954965756@qq.com>
2026-06-09 23:45:52 +0800

    update

--- data_preprocess/bci_2a_preprocess/preprocess.py
+++ data_preprocess/bci_2a_preprocess/preprocess.py
@@ -5,11 +5,84 @@ sys.path.append('/bench-mark')
 import mne
 import numpy as np
 import pickle
+from scipy.io import loadmat
 
 from data_preprocess.bci_2a_preprocess.config import PreprocessArgs
-from data_preprocess.utils import _split_subjects, _merge_data
+from data_preprocess.utils import _split_subjects
 
 # left_hand = 769, right_hand = 770, foot = 771, tongue = 772
+def _find_eval_label_file(root_path, file):
+    stem = os.path.splitext(file)[0]
+    candidates = [
+        os.path.join(root_path, f'{stem}.mat'),
+        os.path.join(root_path, 'true_labels2a', f'{stem}.mat'),
+        os.path.join(root_path, 'true_labels', f'{stem}.mat'),
+        os.path.join(root_path, 'true_label', f'{stem}.mat'),
+        os.path.join(root_path, 'labels', f'{stem}.mat'),
+    ]
+    for path in candidates:
+        if os.path.exists(path):
+            return path
+
+    raise FileNotFoundError(
+        f'Cannot find true label .mat for evaluation file {file}. '
+        f'Expected one of: {candidates}'
+    )
+
+
+def _load_eval_labels(root_path, file):
+    label_path = _find_eval_label_file(root_path, file)
+    mat = loadmat(label_path)
+    if 'classlabel' not in mat:
+        raise KeyError(f'{label_path} does not contain "classlabel"')
+
+    labels = np.asarray(mat['classlabel']).reshape(-1).astype(int) - 1
+    return labels
+
+
+def _load_gdf_epochs(root_path, file, channel_keys, label_dict):
+    path = os.path.join(root_path, file)
+    raw = mne.io.read_raw_gdf(path, stim_channel="auto", verbose='ERROR',
+                            exclude=(["EOG-left", "EOG-central", "EOG-right"]))
+    raw.rename_channels(channel_keys)
+
+    events, events_id = mne.events_from_annotations(raw)
+    event_ids = events[:, 2].astype(str)
+
+    if 'T' in file:
+        keys = events_id.keys() & label_dict.keys()
+        new_dict = {}
+        for key in keys:
+            new_dict[key] = events_id[key]
+        mask = np.isin(event_ids, [str(i) for i in list(new_dict.values())])
+        labels = None
+    elif 'E' in file:
+        if '783' not in events_id:
+            raise KeyError(f'{file} does not contain evaluation cue annotation "783"')
+        new_dict = {'783': events_id['783']}
+        mask = event_ids == str(events_id['783'])
+        labels = _load_eval_labels(root_path, file)
+    else:
+        raise ValueError(f'Unsupported BCI-2a file name: {file}')
+
+    events = events[mask]
+    tmin, tmax = 1., 4.
+    epochs = mne.Epochs(raw, events, event_id=new_dict, 
+                            tmin=tmin, tmax=tmax, 
+                            proj=True, baseline=None, preload=True)
+    datas = epochs.get_data()[:, :, :750]
+    datas = np.array(datas, dtype=np.float32)
+
+    if labels is None:
+        labels = epochs.events[:, -1]-events_id['769']
+    elif len(labels) != len(datas):
+        raise ValueError(
+            f'{file} has {len(datas)} evaluation epochs but {len(labels)} labels in the .mat file'
+        )
+
+    return datas, labels
+
+
 def get_data_all(root_path, data_save_dir):
     label_dict = {'769': 7, '770': 8, '771': 9, '772': 10}
     channel_keys = {'EEG-Fz': 'Fz', 'EEG-0': 'FC3', 'EEG-1': 'FC1', 'EEG-2': 'FCz', 'EEG-3': 'FC2', 'EEG-4': 'FC4',
@@ -24,32 +97,23 @@ def get_data_all(root_path, data_save_dir):
         with open(channel_file, 'w') as f:
             json.dump(ch_names, f)
 
-    for file in os.listdir(root_path):
-        if 'T' not in file:
+    subject_data = {}
+    subject_labels = {}
+    for file in sorted(os.listdir(root_path)):
+        if not file.endswith('.gdf') or ('T' not in file and 'E' not in file):
             continue
 
-        path = os.path.join(root_path, file)
-        raw = mne.io.read_raw_gdf(path, stim_channel="auto", verbose='ERROR',
-                                exclude=(["EOG-left", "EOG-central", "EOG-right"]))
-        raw.rename_channels(channel_keys)
+        subject = file[:3]
+        datas, labels = _load_gdf_epochs(root_path, file, channel_keys, label_dict)
+        subject_data.setdefault(subject, []).append(datas)
+        subject_labels.setdefault(subject, []).append(labels)
 
-        events, events_id = mne.events_from_annotations(raw)
-        event_ids = events[:, 2].astype(str)
-        keys = events_id.keys() & label_dict.keys()
-        new_dict = {}
-        for key in keys:
-            new_dict[key] = events_id[key]
-        mask = np.isin(event_ids, [str(i) for i in list(new_dict.values())])
-        events = events[mask]
-        tmin, tmax = 1., 4.
-        epochs = mne.Epochs(raw, events, event_id=new_dict, 
-                                tmin=tmin, tmax=tmax, 
-                                proj=True, baseline=None, preload=True)
-        labels = epochs.events[:, -1]-events_id['769']
-        datas = epochs.get_data()[:, :, :750]
-        datas = np.array(datas, dtype=np.float32)
-        np.save(os.path.join(data_save_dir, f'{file[:3]}_data.npy'), datas)
-        np.save(os.path.join(data_save_dir, f'{file[:3]}_label.npy'), labels)
+    for subject in sorted(subject_data.keys()):
+        datas = np.concatenate(subject_data[subject], axis=0)
+        labels = np.concatenate(subject_labels[subject])
+        np.save(os.path.join(data_save_dir, f'{subject}_data.npy'), datas)
+        np.save(os.path.join(data_save_dir, f'{subject}_label.npy'), labels)
+        print(f'data and label of subject {subject} saved')
 
 
 def group_data(args):
